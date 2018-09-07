164be469d3ad9f17bd472eb9694242a100c27675
zhhgao <zhhgao@mobvoi.com>
2018-09-07 17:14:56 +0800

    精简spark提交参数，保留必要参数

--- src/main/java/com/mobvoi/assembly/run.sh
+++ src/main/java/com/mobvoi/assembly/run.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
 JAR_PATH=persona-1.0-SNAPSHOT.jar
 
-spark2-submit --class com.mobvoi.TestHive  \
+spark2-submit \
+--class com.mobvoi.TestHive \
 --master yarn \
---executor-memory 1G \
---driver-memory 2G \
---conf spark.default.parallelism=100 \
---total-executor-cores 30 \
---executor-cores 2 \
---deploy-mode client --jars /spark-test/persona-1.0-SNAPSHOT/lib/*.jar $JAR_PATH
-
-#jars 后面跟的路径为hdfs的相对路径 再后面跟要执行的spark job jar
-#--total-executor-cores 30 \ 这个配置貌似只在Spark standalone and Mesos生效，提交时候测试一下去掉这个参数
-#--conf spark.default.parallelism=100 \ 这个配置提交时候测试一下去掉：https://spark.apache.org/docs/latest/configuration.html
+--deploy-mode client \
+--jars /spark-test/persona-1.0-SNAPSHOT/lib/*.jar $JAR_PATH \
