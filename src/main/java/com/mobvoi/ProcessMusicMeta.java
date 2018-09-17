0e150c37f3f3aa12ed26f4e071e93602bf3fa729
zhhgao <zhhgao@mobvoi.com>
2018-09-17 10:17:05 +0800

    增加处理音乐meta信息spark job

---
+++ src/main/java/com/mobvoi/ProcessMusicMeta.java
@@ -0,0 +1,13 @@
+// Copyright 2018 Mobvoi Inc. All Rights Reserved.
+
+package com.mobvoi;
+
+/**
+ * 处理音乐meta信息 spark job created by zhhgao@mobvoi.com on 2018/9/14
+ */
+public class ProcessMusicMeta {
+
+  public static void main(String[] args) {
+
+  }
+}
